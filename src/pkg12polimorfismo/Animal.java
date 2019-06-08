13a03f8233e368f6f233022165b44a1a1d3c74c4
J4R84S <jarbasstm@hotmail.com>
2019-06-08 20:52:04 -0300

    Atualizado em 29052019

---
+++ src/pkg12polimorfismo/Animal.java
@@ -0,0 +1,12 @@
+
+package pkg12polimorfismo;
+public abstract class Animal {
+    // Atributos
+    protected float peso;
+    protected int indade;
+    protected int menbro;
+    // Metodos Abstratos
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
+}
